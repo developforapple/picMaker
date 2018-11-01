214628eeebc4fac8991378e70ddc66eabcaff7b2
tiny <developforapple@163.com>
2018-11-01 12:02:48 +0800

    update

---
+++ AppStorePicMaker/AppStorePicMaker/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  AppStorePicMaker
+//
+//  Created by Jay on 2017/12/6.
+//  Copyright © 2017年 tiny. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+}
+
+@end
